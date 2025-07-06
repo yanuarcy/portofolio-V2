3e9a31a14bb5985cf794144992af4822ea203805
Yanuar <yanuarcahyo@gmail.com>
2025-07-06 16:05:15 +0700

    update script route vercel config V2

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'throw' => false,
         ],
 
+        'vercel' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
